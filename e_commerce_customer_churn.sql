ec398f68711fa2560402f8bf748633cb2554855e
Elvis DARKO <elvis_darko@outlook.com>
2024-01-19 01:04:00 +0000

    sql file updated

--- e_commerce_customer_churn.sql
+++ e_commerce_customer_churn.sql
@@ -9,13 +9,48 @@ products and customers who have stopped using the company's products.
 /**********************
 DATA CLEANING
 **********************/
+
+-- Correct the name of table
+RENAME table ecommerce_churn to ecommercechurn;
+
 -- Correct the name of inconsistent column
-ALTER table ecommerce_churn
+ALTER table ecommercechurn
 RENAME COLUMN ï»¿CustomerID TO CustomerID;
 
 --  Check for duplicate rows
 SELECT CustomerID, COUNT(CustomerID) as Count
-FROM ecommerce_churn
+FROM ecommercechurn
 GROUP BY CustomerID
-Having COUNT(CustomerID) > 1
+Having COUNT(CustomerID) > 1;
 -- Answer = There are no duplicate rows
+
+--  Check columns for null values --
+SELECT 'Tenure' as ColumnName, COUNT(*) AS NullCount 
+FROM ecommercechurn
+WHERE Tenure IS NULL 
+UNION
+SELECT 'WarehouseToHome' as ColumnName, COUNT(*) AS NullCount 
+FROM ecommercechurn
+WHERE warehousetohome IS NULL 
+UNION
+SELECT 'HourSpendonApp' as ColumnName, COUNT(*) AS NullCount 
+FROM ecommercechurn
+WHERE hourspendonapp IS NULL
+UNION
+SELECT 'OrderAmountHikeFromLastYear' as ColumnName, COUNT(*) AS NullCount 
+FROM ecommercechurn
+WHERE orderamounthikefromlastyear IS NULL 
+UNION
+SELECT 'CouponUsed' as ColumnName, COUNT(*) AS NullCount 
+FROM ecommercechurn
+WHERE couponused IS NULL 
+UNION
+SELECT 'OrderCount' as ColumnName, COUNT(*) AS NullCount 
+FROM ecommercechurn
+WHERE ordercount IS NULL 
+UNION
+SELECT 'DaySinceLastOrder' as ColumnName, COUNT(*) AS NullCount 
+FROM ecommercechurn
+WHERE daysincelastorder IS NULL;
+-- There are no null values in the dataset
+
